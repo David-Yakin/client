639f1a7138955cef346284cc9e6761fedc1eb407
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2022-11-08 16:08:50 +0200

    adding material ui & material-icons & Roboto font

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Button } from "@mui/material";
+import { Add } from "@mui/icons-material";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1> app works!</h1>
+      <Button
+        variant="outlined"
+        startIcon={<Add />}
+        sx={{
+          margin: 2,
+        }}>
+        Outlined
+      </Button>
+    </div>
+  );
 }
 
 export default App;
