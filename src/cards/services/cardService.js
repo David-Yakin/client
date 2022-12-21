23f6063366d0fd0e887565ac3790ac390e47e0be
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2022-12-21 12:44:08 +0200

    cardService fixed url

--- src/cards/services/cardService.js
+++ src/cards/services/cardService.js
@@ -4,7 +4,7 @@ const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:8181";
 
 export const getCards = async () => {
   try {
-    const { data } = await axios.get(`${apiUrl}/cardsfff`);
+    const { data } = await axios.get(`${apiUrl}/cards`);
     return data;
   } catch (error) {
     return Promise.reject(error.message);
